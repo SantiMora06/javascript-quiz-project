07a7314fd1ca4799f77ae4f42f9388352a12b1b7
maria magdalena <neumariamagdalena@gmail.com>
2024-06-10 15:27:21 +0200

    stupid commit

--- src/question.js
+++ src/question.js
@@ -1,17 +1,13 @@
 class Question {
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-constructor (text, choices, answer, difficulty){
-this.text = text;
-this.choices = choices;
-this.answer = answer;
-this.difficulty = difficulty;
-
+  shuffleChoices() {
+    const shuffledChoices = this.choices.sort((a, b) => 0.5 - Math.random());
+    return shuffledChoices;
+  }
 }
-
-   shuffleChoices(){
-    
-    const shuffledChoices =  this.choices.sort((a, b) => 0.5 - Math.random());
-   return shuffledChoices;
-}
-
-   }
